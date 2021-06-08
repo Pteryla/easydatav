62c53ba419306b6e065e674e82569303ce1ea0d6
Wong YiChen <1573578457@qq.com>
2021-06-09 00:09:43 +0800

    workbench size position finished

--- src/store/workbench.js
+++ src/store/workbench.js
@@ -1,48 +1,48 @@
 const state = () => ({
-    screen: {
-        position: {
-            left: 20,
-            top: 20,
-        },
-        size: {
-            width: 800,
-            height: 100,
-        },
-        sizeRation: {
-            widthRation: 100,
-            heightRation: 100,
-        },
-        backgroundColor: '#ccc',
+  screen: {
+    position: {
+      left: 20,
+      top: 20,
     },
+    size: {
+      width: 350,
+      height: 600,
+    },
+    sizeRatio: {
+      widthRatio: 100,
+      heightRatio: 100,
+    },
+    backgroundColor: '#ccc',
+  },
 });
 
 const getters = {};
 const actions = {};
 const mutations = {
-    set_position(state, position) {
-        let { left, top } = position;
-        state.screen.position.left = left;
-        state.screen.position.top = top;
-    },
-    set_size(state, size) {
-        let { width, height } = size;
-        state.screen.size.width = width;
-        state.screen.size.height = height;
-    },
-    set_sizeRation(state, sizeRation) {
-        let { widthRation, heightRation } = sizeRation;
-        state.screen.sizeRation.widthRation = widthRation;
-        state.screen.sizeRation.heightRation = heightRation;
-    },
-    set_backgroundColor(state, color) {
-        state.screen.backgroundColor = color;
-    },
+  set_position(state, position) {
+    let { left, top } = position;
+    state.screen.position.left = left;
+    state.screen.position.top = top;
+  },
+  set_size(state, size) {
+    let { width, height } = size;
+    state.screen.size.width = width;
+    state.screen.size.height = height;
+  },
+  set_sizeRatio(state, sizeRatio) {
+    let { widthRatio, heightRatio } = sizeRatio;
+    state.screen.sizeRatio.widthRatio = widthRatio;
+    state.screen.sizeRatio.heightRatio = heightRatio;
+  },
+  set_backgroundColor(state, color) {
+    state.screen.backgroundColor = color;
+  },
 };
 
 export default {
-    namespace: true,
-    state,
-    getters,
-    mutations,
-    actions,
+  namespace: true,
+  state,
+  getters,
+  mutations,
+  actions,
 };
--- src/views/Workbench.vue
+++ src/views/Workbench.vue
@@ -10,35 +10,7 @@
           <workbench-left-menu></workbench-left-menu>
         </el-aside>
         <el-main>
-          <div class="ieasydatav—workbench" id="ieasydatav—workbench">
-            <div class="scale-rulers">
-              <div class="ruler-top">
-                <div
-                    class="ruler-top-item"
-                    :style="{ width: rulerUnit + 'px' }"
-                    v-for="(item, index) in rulerTopItemList"
-                    :key="index"
-                >
-                  <p>{{ item }}</p>
-                </div>
-              </div>
-              <div class="ruler-left">
-                <div
-                    class="ruler-left-item"
-                    :style="{ height: rulerUnit + 'px' }"
-                    v-for="(item, index) in rulerLeftItemList"
-                    :key="index"
-                >
-                  <p>{{ item }}</p>
-                </div>
-              </div>
-            </div>
-            <div class="screen-container">
-              <div class="screen-wrapper">
-                <div class="screen">haha</div>
-              </div>
-            </div>
-          </div>
+          <workbench-screen></workbench-screen>
         </el-main>
         <el-aside width="270px">
           <workbench-right-menu></workbench-right-menu>
@@ -48,67 +20,34 @@
   </div>
 </template>
 <script>
-import {mapState} from 'vuex'
-import WorkbenchHeader from "../components/workbenchComponents/WorkbenchHeader";
-import WorkbenchRightMenu from "../components/workbenchComponents/WorkbenchRightMenu";
-import WorkbenchLeftMenu from "../components/workbenchComponents/WorkbenchLeftMenu";
+import { mapState } from 'vuex';
+import WorkbenchHeader from '../components/workbenchComponents/WorkbenchHeader';
+import WorkbenchRightMenu from '../components/workbenchComponents/WorkbenchRightMenu';
+import WorkbenchLeftMenu from '../components/workbenchComponents/WorkbenchLeftMenu';
+import WorkbenchScreen from '../components/workbenchComponents/WorkbenchScreen';
 export default {
   name: 'Workbench',
   components: {
+    WorkbenchScreen,
     WorkbenchLeftMenu,
     WorkbenchRightMenu,
     WorkbenchHeader,
-
   },
-  setup() {
-  },
+  setup() {},
   data() {
-    return {
-      screenSizeRatio: 100,
-      rulerUnit: 100,
-      rulerTopItemList: [],
-      rulerLeftItemList: [],
-
-    };
+    return {};
   },
-  created() {
-
-  },
-  beforeMount() {
-  },
-  mounted() {
-    this.setScaleRulers();
-    console.log(this.rulerTopItemList);
-    console.log(this.rulerLeftItemList);
-  },
-  unmounted() {
-  },
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  unmounted() {},
   watch: {},
   computed: {
     ...mapState({
-      screen: state => state.workbench.screen
-    })
+      screen: state => state.workbench.screen,
+    }),
   },
-  methods: {
-
-
-    setScaleRulers() {
-      this.rulerTopItemList = [];
-      this.rulerLeftItemList = [];
-      let ieasydatavWorkbench = document.getElementById('ieasydatav—workbench');
-      if (ieasydatavWorkbench) {
-        let {scrollHeight, scrollWidth} = ieasydatavWorkbench;
-        let unit_top = Math.ceil((scrollHeight - 20) / this.rulerUnit);
-        let unit_left = Math.ceil((scrollWidth - 20) / this.rulerUnit);
-        for (let i = 0; i <= unit_top + 10; i++) {
-          this.rulerTopItemList.push(Math.round(i * this.rulerUnit * (100 / this.screenSizeRatio)));
-        }
-        for (let i = 0; i <= unit_left + 10; i++) {
-          this.rulerLeftItemList.push(Math.round(i * this.rulerUnit * (100 / this.screenSizeRatio)));
-        }
-      }
-    },
-  },
+  methods: {},
 };
 </script>
 
@@ -131,8 +70,6 @@ export default {
     user-select: none;
     box-sizing: border-box;
     background-color: $background-color-main;
-
-
   }
 
   .el-aside {
@@ -146,141 +83,6 @@ export default {
     box-sizing: border-box;
     border-left: 1px solid rgb(0, 0, 0);
     border-right: 1px solid rgb(0, 0, 0);
-
-    .ieasydatav—workbench {
-      width: 100%;
-      height: 100%;
-      position: relative;
-
-      .scale-rulers {
-        position: relative;
-
-        .ruler-top {
-          cursor: pointer;
-          position: absolute;
-          z-index: 10;
-          height: 20px;
-          // background-color: $scale-rulers-background-color;
-          top: 0px;
-          left: 20px;
-          display: flex;
-
-          .ruler-top-item {
-            flex-shrink: 0;
-            height: 20px;
-            pointer-events: none;
-            text-align: left;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-            background-image: repeating-linear-gradient(90deg, #606266 0, #606266 1px, transparent 0, transparent 100%),
-            repeating-linear-gradient(90deg, #606266 0, #606266 1px),
-            repeating-linear-gradient(90deg, #606266 0px, #606266 1px, transparent 0, transparent 5%),
-            linear-gradient($scale-rulers-background-color, $scale-rulers-background-color);
-            background-position: 0em 100%, 50% 100%, 0 100%, 0 0, 0 0;
-            background-size: 100% 100%, 1px 53%, 100% 3px, 100% 100%, 100% 100%;
-            background-repeat: no-repeat;
-
-            & p {
-              padding: 0;
-              transform: scale(0.8);
-              font-size: 12px;
-              transform-origin: 0 0;
-              margin: 5px 0 0 5px;
-            }
-          }
-        }
-
-        .ruler-left {
-          cursor: pointer;
-          display: flex;
-          flex-direction: column;
-          position: absolute;
-          width: 20px;
-          z-index: 10;
-          background-color: $scale-rulers-background-color;
-          top: 20px;
-          left: 0px;
-
-          .ruler-left-item {
-            flex-shrink: 0;
-            width: 20px;
-            position: relative;
-            pointer-events: none;
-            text-align: right;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-            background-image: repeating-linear-gradient(
-                    180deg,
-                    #606266 0px,
-                    #606266 1px,
-                    transparent 0px,
-                    transparent 45%
-            ),
-            repeating-linear-gradient(180deg, #606266 0, #606266 1px, transparent 0, transparent 50%),
-            repeating-linear-gradient(180deg, #606266 0px, #606266 1px, transparent 0, transparent 5%),
-            linear-gradient($scale-rulers-background-color, $scale-rulers-background-color);
-            background-position: 0 0, 100% 100%, 100% 100%, 0 0, 0 0;
-            background-size: 100% 284%, 40% 100%, 2px 100%, 100% 100%, 100% 100%;
-            background-repeat: no-repeat;
-
-            & p {
-              margin: 0;
-              font-size: 12px;
-              padding: 0;
-              transform: rotate(90deg) scale(0.8);
-              text-align: left;
-              user-select: none;
-              transform-origin: 0 33%;
-              margin-left: 10px;
-            }
-          }
-        }
-      }
-
-      .screen-container {
-        width: 100%;
-        height: 100%;
-        position: relative;
-        overflow: auto;
-
-        &::-webkit-scrollbar {
-          z-index: 0 !important;
-          width: 2px;
-          height: 2px;
-          background-color: #141517;
-        }
-
-        &::-webkit-scrollbar-corner {
-          z-index: 0;
-          background-color: #141517;
-        }
-
-        &::-webkit-scrollbar-thumb {
-          width: 2px;
-          height: 2px;
-          z-index: 0 !important;
-          background-color: #909399;
-        }
-
-        .screen-wrapper {
-          position: absolute;
-          top: 20px;
-          left: 20px;
-          padding-bottom: 20px;
-          padding-right: 20px;
-
-          .screen {
-            width: 1000px;
-            height: 500px;
-            background-color: rgb(159, 164, 170);
-          }
-        }
-      }
-    }
   }
 }
 </style>
